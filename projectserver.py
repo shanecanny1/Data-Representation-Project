646137e0ef6bf06983c4e039fbf6ac8e2923fab0
shanecanny1 <36367839+shanecanny1@users.noreply.github.com>
2020-01-01 02:01:31 +0000

    Server to DB complete

--- projectserver.py
+++ projectserver.py
@@ -1,74 +1,35 @@
 ## Author: Shane Canny
 ## Title: Data Representation Project 2019
-## Date: 29th Dec 2019
+## Date: 01st Jan 2019
 
 
 from flask import Flask, jsonify, request, abort, make_response
+from zprojectDOA import projectDAO
 ## from flask_cors import CORS
 
 app = Flask(__name__, static_url_path='',static_folder='../')
 #CORS(app)
-Movies = [
-    {
-        "ChartNo":1,
-        "Title" : "Incredibles_2",
-        "Director":"Brad Bird",
-        "Budget":"$200 Million",
-        "Box Office":"$1.243 Billion",
-        "Running Time (Minutes)":118},
-    {
-        "ChartNo":2,
-        "Title":"Jurassic World Fallen Kingdom",
-        "Director":"J.A. Bayona",
-        "Budget":"$187 Million",
-        "Box Office":"$1.309 Billion",
-        "Running Time (Minutes)":128
-    },
-    {
-        "ChartNo":3,
-        "Title":"Deadpool 2",
-        "Director":"David Leitch",
-        "Budget":"$110 Million",
-        "Box Office":"$785 Million ",
-        "Running Time (Minutes)":119
-    },
-    {
-        "ChartNo":4,
-        "Title":"The Grinch",
-        "Director":"Scott Mosier",
-        "Budget":"$75 Million",
-        "Box Office":"$511.6 Million",
-        "Running Time (Minutes)":86
-    },
-    {
-        "ChartNo":5,
-        "Title":"Mission Impossible Fallout",
-        "Director":"Christopher McQuarrie",
-        "Budget":"$178 Million",
-        "Box Office":"$791.1 Million",
-        "Running Time (Minutes)":147
-    },
-]
+
 
 # Retrieving all items of content from the Chart List by using a GET method
-
-@app.route('/Movies', methods=['GET'])
+## Get All Movie Listings
+@app.route('/Movies')
 def get_Movies():
-    return jsonify( {'Movies':Movies})
+    results=projectDAO.getAll()
+    return jsonify(results)
 # curl -i http://127.0.0.1:5000/Movies
 
 # Retrieving individual items of content from the Chart List by using its position in the Chart and using a GET method
 
+#findByChartNo
 @app.route('/Movies/<int:ChartNo>', methods =['GET'])
-def get_Movie(ChartNo):
-    foundMovie = list(filter(lambda t : t['ChartNo'] == ChartNo, Movies))
-    if len(foundMovie) == 0:
-        return jsonify( { 'Movies' : '' }),204
-    return jsonify( { 'Movies' : foundMovie[0] })
+def findByID(ChartNo):
+    foundMovie = projectDAO.findByID(ChartNo)
+    return jsonify(foundMovie)
 #curl -i http://127.0.0.1:5000/Movies/1
 
 # Updating the Chart List by using a POST method i.e. including a new Movie
-
+# Create
 @app.route('/Movies', methods=['POST'])
 def create_Movie():
     if not request.json:
@@ -81,50 +42,55 @@ def create_Movie():
         "Director": request.json['Director'],
         "Budget":request.json['Budget'],
         "Box Office":request.json['Box Office'],
-        "Running Time (Minutes)":request.json['Running Time (Minutes)']
+        "RunningTimeMinutes":request.json['RunningTimeMinutes']
     }
-    Movies.append(Movie)
-    return jsonify( {'Movie':Movie }),201
-# curl -i -H "Content-Type:application/json" -X POST -d "{\"ChartNo\":\"6\",\"Title\":\"We Win Again\",\"Director\":\"Shane Canny\",\"Budget\":\"12\",\"Box Office\":\"105\",\"Running Time (Minutes)\":3000150}" http://127.0.0.1:5000/Movies
+    
+    values = (Movie['ChartNo'], Movie['Title'], Movie['Director'], Movie['Budget'], Movie['Box Office'], Movie['RunningTimeMinutes'])
+    newID = projectDAO.create(values)
+    Movie['ChartNo'] = newID
+    return jsonify(Movie),201
+# curl -i -H "Content-Type:application/json" -X POST -d "{\"ChartNo\":\"6\",\"Title\":\"We Win Again\",\"Director\":\"Shane Canny\",\"Budget\":\"12\",\"Box Office\":\"105\",\"RunningTimeMinutes\":3000150}" http://127.0.0.1:5000/Movies
 
 # Updating the contents of the Chart List by using its position in the Chart using a PUT method 
-
+#Put
 @app.route('/Movies/<int:ChartNo>', methods =['PUT'])
 def update_Movie(ChartNo):
-    foundMovies=list(filter(lambda t : t['ChartNo'] ==ChartNo, Movies))
-    if len(foundMovies) == 0:
+    foundMovie=projectDAO.findByID(ChartNo)
+    if not foundMovie:
         abort(404)
     if not request.json:
         abort(400)
-    if 'Title' in request.json and type(request.json['Title']) != str:
+    if 'ChartNo' in request.json and type(request.json['ChartNo']) is not str:
         abort(400)
-    if 'Director' in request.json and type(request.json['Director']) is not str:
-        abort(400)
-    if 'Budget' in request.json and type(request.json['Budget']) is not str:
-        abort(400)
-    if 'Box Office' in request.json and type(request.json['Box Office']) is not str:
-        abort(400)
-    if 'Running Time (Minutes)' in request.json and type(request.json['Running Time (Minutes)']) is not int:
-        abort(400)
-    foundMovies[0]['Title']  = request.json.get('Title', foundMovies[0]['Title'])
-    foundMovies[0]['Director']  = request.json.get('Director', foundMovies[0]['Director'])
-    foundMovies[0]['Budget']  = request.json.get('Budget', foundMovies[0]['Budget'])
-    foundMovies[0]['Box Office'] =request.json.get('Box Office', foundMovies[0]['Box Office'])
-    foundMovies[0]['Running Time (Minutes)'] =request.json.get('Running Time (Minutes)', foundMovies[0]['Running Time (Minutes)'])
-    return jsonify( {'Movie':foundMovies[0]})
+    if 'Title' in request.json:
+        foundMovie['Title'] = request.json['Title'] 
+
+    if 'Director' in request.json:
+        foundMovie['Director'] = request.json['Director']
+    
+    if 'Budget' in request.json:
+        foundMovie['Budget'] = request.json['Budget']
+    
+    if 'BoxOffice' in request.json:
+        foundMovie['BoxOffice'] = request.json['BoxOffice']
+    
+    if 'RunningTimeMinutes' in request.json:
+        foundMovie['RunningTimeMinutes'] = request.json['RunningTimeMinutes']
+   
+    values = (foundMovie['ChartNo'],foundMovie['Title'], foundMovie['Director'], foundMovie['Budget'], foundMovie['BoxOffice'], foundMovie['RunningTimeMinutes'])
+    
+    projectDAO.update(values)
+    return jsonify(foundMovie),201
 
 #curl -i -H "Content-Type:application/json" -X PUT -d "{\"Director\":\"Niamh Canny\"}" http://127.0.0.1:5000/Movies/2
 
 # Deleting a Movie by using its position in the Chart i.e. ChartNo
 @app.route('/Movies/<int:ChartNo>', methods =['DELETE'])
 def delete_Movie(ChartNo):
-    foundMovies = list(filter (lambda t : t['ChartNo'] == ChartNo, Movies))
-    if len(foundMovies) == 0:
-        abort(404)
-    Movies.remove(foundMovies[0])
+    projectDAO.delete(ChartNo)
     return jsonify( {'Item Deleted':True })
 
-#curl -i -H "Content-Type:application/json" -X "DELETE" -d http://127.0.0.1:5000/Movies/5
+#curl -i -H "Content-Type:application/json" -X "DELETE" http://127.0.0.1:5000/Movies/0
 
 @app.errorhandler(404)
 def not_found404(error):
---
+++ zprojectDOA.py
@@ -0,0 +1,72 @@
+import mysql.connector
+
+class ProjectDAO:
+    db=""
+    def __init__(self): 
+        self.db = mysql.connector.connect(
+        host="localhost",
+        user="root",
+        password="",
+        port=3308,
+        database="project"
+        )
+    
+            
+    def create(self, values):
+        cursor = self.db.cursor()
+        sql="insert into movies (ChartNo, Title, Director, Budget, BoxOffice, RunningTimeMinutes) values (%s,%s,%s,%s,%s,%s)"
+        cursor.execute(sql, values)
+
+        self.db.commit()
+        return cursor.lastrowid
+
+    def getAll(self):
+        cursor = self.db.cursor()
+        sql="select * from movies"
+        cursor.execute(sql)
+        results = cursor.fetchall()
+        returnArray = []
+        print(results)
+        for result in results:
+            print(result)
+            returnArray.append(self.convertToDictionary(result))
+
+        return returnArray
+
+    def findByID(self, ChartNo):
+        cursor = self.db.cursor()
+        sql="SELECT * from movies WHERE ChartNo = %s"
+        values=(ChartNo,)
+
+        cursor.execute(sql, values)
+        result = cursor.fetchone()
+        return self.convertToDictionary(result)
+
+    def update(self, values):
+        cursor = self.db.cursor()
+        sql="update movies set ChartNo=%s,Title=%s,Director=%s,Budget=%s,BoxOffice=%s,RunningTimeMinutes=%s where ChartNo = %s"
+        cursor.execute(sql, values)
+        self.db.commit()
+
+    def delete(self, ChartNo):
+        cursor = self.db.cursor()
+        sql="delete from movies where ChartNo = %s"
+        values = (ChartNo,)
+
+        cursor.execute(sql, values)
+
+        self.db.commit()
+        print("delete done")
+
+    def convertToDictionary(self, result):
+        colnames=['ChartNo','Title','Director', 'Budget','BoxOffice',"RunningTimeMinutes"]
+        item = {}
+        
+        if result:
+            for i, colName in enumerate(colnames):
+                value = result[i]
+                item[colName] = value
+        
+        return item
+        
+projectDAO = ProjectDAO()
